4d36b91af34d1688dc7c107b8802b152104dd3cf
GBR1405 <124008178+GBR1405@users.noreply.github.com>
2025-07-13 03:41:02 -0600

    diseños

--- job-portal-frontend/src/games/DrawingLiveViewer.js
+++ job-portal-frontend/src/games/DrawingLiveViewer.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import "../styles/LiveViewDrawings.css";
 
 const ProfessorDrawingViewer = ({ partidaId, socket }) => {
   const [selectedTeam, setSelectedTeam] = useState(null);
